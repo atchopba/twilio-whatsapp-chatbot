744254cf4ce25bc7f547f0c833aed1822ca12a4c
albinTch <atchopba@gmail.com>
2022-10-23 12:12:04 +0200

    Add questions

--- twilio_whatsapp_bot/core/loan_steps.py
+++ twilio_whatsapp_bot/core/loan_steps.py
@@ -4,32 +4,61 @@ from typing import Any
 
 DEVISE = "Fcfa"
 BAD_ANSWER_STR = "mauvaise réponse"
-
+user_responses = {}
 
 def step_response(incoming_msg: str) -> str:
-    global previous_step
+    global current_step, user_responses
     quote = ""
-    response_ = incoming_msg.lower().strip()
+    response_ = incoming_msg.strip()
     # 
-    if previous_step == 1:
+    if current_step == 1:
         if response_ not in { "1", "2", "3", "4", "5" }:
             quote = BAD_ANSWER_STR
         else:
+            user_responses[current_step] = response_
             quote = step_second()
-    elif previous_step == 2:
+    elif current_step == 2:
         if response_ not in { "a", "b", "c", "d", "e" }:
             quote = BAD_ANSWER_STR
         else:
-            quote = step_third()
+            user_responses[current_step] = response_
+            quote = step_three()
+    elif current_step == 3:
+        if response_ not in { "1", "2" }:
+            quote = BAD_ANSWER_STR
+        else:
+            user_responses[current_step] = response_
+            quote = step_four()
+    elif current_step == 4:
+        # check name
+        response_tokens = response_.split(" ")
+        if response_.isnumeric() or len(response_tokens[0]) > 10:
+            quote = BAD_ANSWER_STR
+        else:
+            user_responses[current_step] = response_
+            quote = step_five()
+    elif current_step == 5:
+        # check phone number
+        if not response_.isnumeric():
+            quote = BAD_ANSWER_STR
+        else:
+            user_responses[current_step] = response_
+            quote = step_six()
+    elif current_step == 6:
+        # check city & municipality
+        if response_.isnumeric(): 
+            quote = BAD_ANSWER_STR
+        else:
+            quote = "Merci pour vos réponses, M./Mme %s. Nous rentrerons en contact avec vous sous un délai de 48h maximum." % (user_responses[4].upper())
     else:
         quote = "l'étape précédente n'a pas été enregistrée"
     return quote
 
 
 def step_first() -> str:
-    global previous_step
+    global current_step
     # initialize previous step
-    previous_step = 1
+    current_step = 1
     # make question 
     quote = "Quel montant souhaitez-vous emprunter? \n"
     quote += "1. 5 millions %s \n"  % (DEVISE)
@@ -42,9 +71,9 @@ def step_first() -> str:
 
 
 def step_second() -> str:
-    global previous_step
+    global current_step
     # initialize previous step
-    previous_step = 2
+    current_step = 2
     # make question
     quote = "En combien de temps comptez-vous rembourser? \n"
     quote += "A. 5 ans \n"
@@ -55,12 +84,39 @@ def step_second() -> str:
     return quote
 
 
-def step_third() -> str:
-    global previous_step
+def step_three() -> str:
+    global current_step
     # initialize previous step
-    previous_step = 3
+    current_step = 3
     # make question
     quote = "Avez-vous des garanties? \n"
     quote += "1. Oui \n"
     quote += "2. Non \n"
+    return quote
+
+
+def step_four() -> str:
+    global current_step
+    # initialize previous step
+    current_step = 4
+    # make question
+    quote = "Vos nom(s) et prénom(s):"
+    return quote
+
+
+def step_five() -> str:
+    global current_step
+    # initialize previous step
+    current_step = 5
+    # make question
+    quote = "Votre numéro de téléphone"
+    return quote
+
+
+def step_six() -> str:
+    global current_step
+    # initialize previous step
+    current_step = 6
+    # make question
+    quote = "Votre quartier de résidence / commune"
     return quote
