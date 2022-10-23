acc69e505a2906043a879ea907cce6114d7c4117
albinTch <atchopba@gmail.com>
2022-10-23 12:11:36 +0200

    Fix string

--- twilio_whatsapp_bot/routes.py
+++ twilio_whatsapp_bot/routes.py
@@ -6,10 +6,10 @@ from .core.loan_steps import step_first, step_response
 
 
 # Blueprint Configuration
-main_bp = Blueprint('main_bp', 
+main_bp = Blueprint("main_bp", 
                     __name__,
-                    template_folder='templates',
-                    static_folder='static')
+                    template_folder="templates",
+                    static_folder="static")
 
 @main_bp.route("/")
 def hello():
@@ -19,7 +19,7 @@ def hello():
 @main_bp.route("/bot", methods=["POST"])
 def bot():
     # add webhook logic here and return a response
-    incoming_msg = request.values.get('Body', '').lower()
+    incoming_msg = request.values.get("Body", "").lower().strip()
     resp = MessagingResponse()
     msg = resp.message()
     
