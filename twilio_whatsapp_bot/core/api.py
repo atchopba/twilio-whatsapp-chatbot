23744545f844ee64994a38021f3c52c9abece3e6
albinTch <atchopba@gmail.com>
2023-03-16 00:04:15 +0100

    Refactoring algo

--- twilio_whatsapp_bot/core/api.py
+++ twilio_whatsapp_bot/core/api.py
@@ -4,40 +4,72 @@ from twilio_whatsapp_bot.core.helpers import get_file_content, get_list_files
 
 
 BAD_ANSWER_STR = "mauvais choix de réponse"
-PATHDIR_QUESTIONS = "./data/questions"
+PATHDIR_DIALOG = "./data/dialog"
 user_responses = {}
 
-list_files = get_list_files(PATHDIR_QUESTIONS)
+list_files = get_list_files(PATHDIR_DIALOG)
+is_last_dialog = False
 
 
 def step_question(index: int) -> str:
-    global current_step, user_responses
+    global current_step, user_responses, is_last_dialog
+    list_files_size = len(list_files)
     # verify if the index is the last question
-    tmp_file = str(index) + ".txt"
-    if tmp_file == list_files[len(list_files)-2]:
-        quote = get_file_content(PATHDIR_QUESTIONS + "/z.txt").replace("%s", user_responses[4].upper())
+    if index == list_files_size - 1:
+        is_last_dialog = True
+        quote = get_file_content(list_files[list_files_size - 1]).replace("{}", user_responses[list_files_size - 2].upper())
         # store data in DB
         Answers().insert_data(user_responses)
     else:
+        is_last_dialog = False
         current_step = index
-        quote = get_file_content(PATHDIR_QUESTIONS + "/" + list_files[index])
+        quote = get_file_content(list_files[index])
     return quote
 
 
 def step_response(incoming_msg: str) -> str:
+    global current_step, user_responses, is_last_dialog
+    response_msg = incoming_msg.strip()
+    current_file = list_files[current_step]
+    is_question = False
+    # if question is a courtesy
+    if current_step == 0 or "courtesy" in current_file:
+        quote = step_in_courtesy(response_msg)
+    # else
+    elif "question" in current_file:
+        quote = step_in_question(response_msg)
+    return {
+        "quote" : quote,
+        "is_last_dialog" : is_last_dialog
+    }
+
+
+def step_in_courtesy(response_msg: str) -> str:
     global current_step, user_responses
-    response_ = incoming_msg.strip()
-    current_file = PATHDIR_QUESTIONS + "/" + str(current_step) + ".txt"
+    user_responses[current_step] = response_msg
+    next_courtesy_content = get_file_content(list_files[current_step + 1])
+    # check if the content contains {} to replace with the response
+    if ("{}" in next_courtesy_content):
+        next_courtesy_content = next_courtesy_content.replace("{}", response_msg)
+    #
+    current_step += 1
+    return next_courtesy_content
+
+
+def step_in_question(response_msg: str) -> str:
+    global current_step, user_responses
+    quote = ""
+    current_file = list_files[current_step]
     # get nb of line of the current file
     nb_lines = len(get_file_content(current_file).split("\n"))
     # if file is on 1 line, get the answer and next question
     if nb_lines == 1:
-        user_responses[current_step] = response_
+        user_responses[current_step] = response_msg
         quote = step_question(current_step + 1)
     # nb line > 1, many possibilities of responses
     else:
-        if response_.isnumeric() and 1 <= int(response_) <= nb_lines-1:
-            user_responses[current_step] = response_
+        if response_msg.isnumeric() and 1 <= int(response_msg) <= nb_lines-1:
+            user_responses[current_step] = response_msg
             quote = step_question(current_step + 1)
         else:
             current_step = current_step
--- twilio_whatsapp_bot/routes.py
+++ twilio_whatsapp_bot/routes.py
@@ -2,9 +2,13 @@
 from flask import Blueprint, request, render_template
 # import requests
 from twilio.twiml.messaging_response import MessagingResponse
-from .core.api import step_question, step_response, PATHDIR_QUESTIONS
+from .core.api import step_question, step_response, PATHDIR_DIALOG
 from .core.helpers import get_file_content
 
+global is_begin_dialog
+
+is_begin_dialog = True
+current_step = 0
 
 # Blueprint Configuration
 main_bp = Blueprint("main_bp", 
@@ -19,16 +23,21 @@ def hello():
 
 @main_bp.route("/bot", methods=["POST"])
 def bot():
+    global is_begin_dialog
     ## print("===> request : ", request.values)
     # add webhook logic here and return a response
     incoming_msg = request.values.get("Body", "").lower().strip()
     resp = MessagingResponse()
     msg = resp.message()
-    
-    if get_file_content(PATHDIR_QUESTIONS + "/0.txt") in incoming_msg:
-        msg.body(f"{step_question(1)}")
+
+    if is_begin_dialog:
+        # the initial step is 0
+        msg.body(f"{step_question(0)}")
+        is_begin_dialog = False
     
     else:
-        msg.body(f"{step_response(incoming_msg)}")
+        step_response_ = step_response(incoming_msg)
+        msg.body(f"{step_response_['quote']}")
+        is_begin_dialog = step_response_['is_last_dialog']
     
     return str(resp)
