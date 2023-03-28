7c45e2814a682a5a6adaa2851c968caa73a4b437
atchopba <atchopba@gmail.com>
2023-03-28 23:09:29 +0200

    Added the possibility of free dialogue

--- twilio_whatsapp_bot/core/api.py
+++ twilio_whatsapp_bot/core/api.py
@@ -29,6 +29,7 @@ previous_step_str = "courtesy"
 propose_all_questions_folder = ""
 is_change_folder = False
 is_global_question = False
+is_words_question = False
 nb_folder_question = 0
 
 
@@ -64,18 +65,18 @@ def step_question(index: int) -> str:
 
 
 def step_response(incoming_msg: str) -> str:
-    global current_step, user_responses, previous_step_str, is_last_dialog, list_files, is_global_question
+    global current_step, user_responses, previous_step_str, is_last_dialog, is_words_question, list_files, is_global_question
     response_msg = incoming_msg.strip()
     current_file = list_files[current_step]
     quote = ""
     
     # if question is a courtesy
-    if (current_step == 0 or COURTESY_STR in current_file) and not is_change_folder:
+    if ((current_step == 0 or COURTESY_STR in current_file) and not is_change_folder) or is_words_question:
         quote = step_in_courtesy(response_msg)
         logging.info("Dialog is a courtesy")
     # else
     elif QUESTION_STR in current_file and not is_global_question:
-        quote = step_in_question(response_msg)
+        quote = step_in_question(response_msg) 
         previous_step_str = QUESTION_STR
         logging.info("Dialog is a question")
     # if it is global question case
@@ -95,43 +96,55 @@ def step_response(incoming_msg: str) -> str:
 
 
 def step_in_courtesy(response_msg: str) -> str:
-    global current_step, user_responses, list_files, is_change_folder, is_global_question, nb_folder_question, propose_all_questions_folder
+    global current_step, user_responses, list_files, is_change_folder, is_global_question, is_words_question, nb_folder_question, propose_all_questions_folder
     user_responses[current_step] = response_msg
-    next_courtesy_content = get_file_content(list_files[current_step + 1])
-    next_file = list_files[current_step + 1]
-    
+    next_file = ""
+
+    if not is_words_question:
+        next_courtesy_content = get_file_content(list_files[current_step + 1])
+        next_file = list_files[current_step + 1]
+        # check if the content contains {} to replace with the response
+        if ("{}" in next_courtesy_content):
+            next_courtesy_content = next_courtesy_content.replace("{}", response_msg)
+        current_step += 1
     logging.info("step in courtesy {%s}", response_msg)
-    # check if the content contains {} to replace with the response
-    if ("{}" in next_courtesy_content):
-        next_courtesy_content = next_courtesy_content.replace("{}", response_msg)
-    #
-    current_step += 1
+    
     
     # check if the previous step is courtesy
-    if COURTESY_STR not in next_file and not is_change_folder:
+    if (COURTESY_STR not in next_file and not is_change_folder) or is_words_question:
         is_change_folder = True
         folder_ = Folder(load_json_file())
-        folder_question = folder_.check_probability_and_return_folder(response_msg)
+        response_question = folder_.check_probability_and_return_folder(response_msg)
+        folder_index = response_question["folder_index"]
+        question_ = response_question["question"]
+ 
         # if the user's response matches a directory
-        if folder_question > -1:
+        if folder_index > -1 and (question_ is None or question_ == ""):
             # reload list_files
-            list_files = get_list_files(PATHDIR_QUESTIONS + "/" + str(folder_question))
+            list_files = get_list_files(PATHDIR_QUESTIONS + "/" + str(folder_index))
             current_step = 0
+            is_words_question = False
             #
             next_courtesy_content = get_file_content(list_files[current_step])
+            
+        #
+        elif folder_index == -1 and question_ is not None and question_ != "":
+            next_courtesy_content = question_
+            is_words_question = True
         # else, propose all directory
         else:
+            is_words_question = False
             is_global_question = True
             global_question_as_default = folder_.get_folders_as_default()
             nb_folder_question = len(global_question_as_default)
             next_courtesy_content = PROPOSE_ALL_QUESTIONS_FOLDER_STR + "\n\n" + "\n".join(global_question_as_default)
             propose_all_questions_folder = next_courtesy_content
-    #       
+    #   
     return next_courtesy_content
 
 
 def step_in_question(response_msg: str) -> str:
-    global current_step, user_responses
+    global current_step, is_words_question, user_responses
     quote = ""
     current_file = list_files[current_step]
     current_file_content = get_file_content(current_file)
@@ -160,6 +173,10 @@ def step_in_question(response_msg: str) -> str:
         elif operations != "" and operations is not None and not check_msg_validity(response_msg, operations):
             current_step = current_step
             quote = BAD_ANSWER_STR + "\n\n" + current_file_content
+        elif is_words_question:
+            is_words_question = False
+            current_step = current_step
+            quote = current_file_content
         else:
             current_step = current_step
             quote = BAD_ANSWER_CHOICE_STR + "\n\n" + current_file_content
--- twilio_whatsapp_bot/core/utilies/folder.py
+++ twilio_whatsapp_bot/core/utilies/folder.py
@@ -10,20 +10,27 @@ class Folder(object):
 
 
     def check_probability_and_return_folder(self, sentence: str, column_array: str = "words") -> Any:
-        index = -1
+        folder_index = -1
+        question = None
         res_index = -1
         #
         for i in range(0, len(self.data_array)):
             data_ = self.data_array[i]
-            list_A = data_[column_array].split("-")
+            list_A = data_[column_array].split("|")
             res = 0
             for key in list_A:
-                res += 1 if key in sentence else 0
+                tmp_key = str(key).strip()
+                res += 1 if tmp_key in sentence else 0
             if res > 0 and res_index < res:
                 res_index = res
-                index = int(data_["folder"])
+                folder_index = int(data_["folder"]) if data_["folder"] is not None else -1
+                question = data_["question"]
+                
         #    
-        return index
+        return {
+            "folder_index": folder_index,
+            "question": question
+        }
 
     
     def get_folders_as_default(self) -> Any:
@@ -31,7 +38,10 @@ class Folder(object):
         i = 1
         # select all folder define in 0.json and propose that to user
         for data_ in self.data_array:
-            return_.append(str(i) + ". " + data_["title"])
+            if data_["folder"] is not None and data_["folder"] != "":
+                folder_index = str(data_["folder"])
+                if folder_index.isnumeric():
+                    return_.append(str(i) + ". " + data_["title"])
             i += 1
         return return_
     
