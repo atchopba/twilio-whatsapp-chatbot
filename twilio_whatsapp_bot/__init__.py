8cdad67e8d2eec0b5d38258c1d92ddf91e2fd165
albinTch <atchopba@gmail.com>
2023-05-29 21:27:11 +0200

    update

--- twilio_whatsapp_bot/__init__.py
+++ twilio_whatsapp_bot/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from flask import Flask
+from . import routes
 
 
 def create_app():
@@ -7,8 +8,6 @@ def create_app():
     app = Flask(__name__, instance_relative_config=False)
     #
     with app.app_context():
-        from . import routes
-        #
         app.register_blueprint(routes.main_bp)
         #
         # if app.config["FLASK_ENV"] == "development":
--- twilio_whatsapp_bot/routes.py
+++ twilio_whatsapp_bot/routes.py
@@ -10,7 +10,7 @@ is_begin_dialog = True
 current_step = 0
 
 # Blueprint Configuration
-main_bp = Blueprint("main_bp", 
+main_bp = Blueprint("main_bp",
                     __name__,
                     template_folder="templates",
                     static_folder="static")
