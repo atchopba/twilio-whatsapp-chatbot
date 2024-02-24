bb2d9c244d45a9269e0c6411452a35002f9d6311
atchopba <atchopba@gmail.com>
2024-02-24 22:26:18 +0100

    Update config.py
    Delete variable not required

--- config.py
+++ config.py
@@ -37,8 +37,6 @@ class Config:
 
     DEFAULT_TIMEZONE = environ.get("DEFAULT_TIMEZONE")
 
-    DEFAULT_CALENDAR_BOOKING_COLOR = environ.get("DEFAULT_CALENDAR_BOOKING_COLOR") # noqa
-
     DEFAULT_MOMO_URL = environ.get("DEFAULT_MOMO_URL")
 
     WORK_ON_SATURDAY = environ.get("WORK_ON_SATURDAY")
