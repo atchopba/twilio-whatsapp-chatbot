4c99ef1ab989fdce1ac1381de4bb1ee20512bb0f
atchopba <atchopba@gmail.com>
2024-02-25 14:49:12 +0100

    Update db-create.sql
    Delete unique constraint to attribute token

--- data/db/db-create.sql
+++ data/db/db-create.sql
@@ -88,7 +88,7 @@ CREATE TABLE `user_activities` (
 DROP TABLE IF EXISTS `user_calendar_events`;
 CREATE TABLE `user_calendar_events` (
 	`id` INT AUTO_INCREMENT,
-	`token` VARCHAR(64) NOT NULL UNIQUE,
+	`token` VARCHAR(64) NOT NULL,
   `patient` VARCHAR(128) NULL,
 	`event_date` VARCHAR(128) DEFAULT NULL,
 	`start_time`TIME DEFAULT NULL,
