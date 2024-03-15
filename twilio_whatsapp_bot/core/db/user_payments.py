957a42bf615d0a5701bdd5f823d36bd4b70918a7
atchopba <atchopba@gmail.com>
2024-03-15 22:36:47 +0100

    Update user_payments.py

--- twilio_whatsapp_bot/core/db/user_payments.py
+++ twilio_whatsapp_bot/core/db/user_payments.py
@@ -7,26 +7,11 @@ class UserPayments(DB):
     def __init__(self):
         pass
 
-    def select_data(self, user_token: str, payment_token: str) -> bool:
+    def is_data_exists(self, user_token: str, payment_token: str) -> bool:
         sql = "SELECT * FROM user_payments WHERE is_confirmed <> '1' AND user_token = '{0}' AND payment_token = '{1}'".format(user_token, payment_token) # noqa
-        try:
-            self.connect()
-            with self.connection.cursor() as cursor:
-                cursor.execute(sql)
-                rows = cursor.fetchall()
-                return len(rows) > 0
-        except Exception as exception:
-            print("Erreur lors de l'execution : ", exception)
-        self.deconnect()
-        return False
+        rows = self.select(sql)
+        return len(rows) > 0 if rows else -1
 
     def update_data(self, user_token: str, payment_token: str) -> int:
         sql = "UPDATE user_payments SET is_confirmed = '1' WHERE is_confirmed <> '1' AND user_token = '{0}' AND payment_token = '{1}'".format(user_token, payment_token) # noqa
-        try:
-            self.connect()
-            with self.connection.cursor() as cursor:
-                cursor.execute(sql)
-                self.connection.commit()
-        except Exception as exception:
-            print("Erreur lors de l'execution : ", exception)
-        self.deconnect()
+        return self.insert_without_datas(sql)
